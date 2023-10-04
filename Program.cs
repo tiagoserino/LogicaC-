3ec18794001540f3d560fa0027f5315932ac690f
tiagoserino <serinotiago@gmail.com>
2023-10-04 16:49:00 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+int a = 3;
+
+if (a < 0)
+{
+    WriteLine($"O número {a} é negativo!");
+}
+else
+{
+    if (a == 0)
+    {
+        WriteLine($"O número {a}é igual a 0!");
+    }
+    else
+    {
+        WriteLine($"O número {a} é positivo!");
+    }
+}
